72daa212c9f1d2fb13ffcb8040744c53b511a2bc
Gaëtan <dapvril.gaetan@gmail.com>
2025-01-03 16:36:26 +0100

    feat: :sparkles: Search bar to search for Pokémon cards
    -Added search bar -Creation of other Pokémon for testing -Adding media

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,25 @@
-import { Component, computed, effect, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, model, signal } from '@angular/core';
 import { PlayingCardComponent } from './components/playing-card/playing-card.component';
 import { Pokemon } from './models/pokemon.model';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { PokemonType } from './utils/pokemon.utils';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PlayingCardComponent, SearchBarComponent],
+  imports: [CommonModule, PlayingCardComponent, SearchBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   pokemons!: Pokemon[];
-  count: number = 0;
-  search = '';
+  search = model('');
 
-  selectedPokemonIndex = signal(1);
-  selectedPokemon = computed(() => {
-    return this.pokemons[this.selectedPokemonIndex()]
+  filteredPokemons = computed(() => {
+    return this.pokemons.filter(pokemon => pokemon.name.includes(this.search()))
   })
 
   constructor() {
-
-
-    effect(() => {
-      console.log(this.selectedPokemon())
-    })
     this.pokemons = []
 
     const pokemon1 = new Pokemon();
@@ -34,7 +27,7 @@ export class AppComponent {
     pokemon1.image = "pokemon/flo.jpg";
     pokemon1.type = PokemonType.PLANT;
     pokemon1.hp = 150;
-    pokemon1.figureCaption = "N°070 Florizzare";
+    pokemon1.figureCaption = "N°0070 Florizzare";
     pokemon1.attackName = "Tempête Florale";
     pokemon1.attackStrength = 130;
     pokemon1.attackDescription = "Tempte Florale inflige des dégâts à tous les Pokémon présents sur le terrain adjacents au lanceur.";
@@ -49,15 +42,30 @@ export class AppComponent {
     pokemon2.attackStrength = 35;
     pokemon2.attackDescription = "Siphon permet de traverser les tourbillons barrant le passage.";
 
+    const pokemon3 = new Pokemon();
+    pokemon3.name = "Raichu";
+    pokemon3.image = "pokemon/raichu.jpg";
+    pokemon3.type = PokemonType.ELECTRIC;
+    pokemon3.hp = 80;
+    pokemon3.figureCaption = "N°0026 Raichu";
+    pokemon3.attackName = "Éclair";
+    pokemon3.attackStrength = 55;
+    pokemon3.attackDescription = "Éclair inflige des dégâts et a 10% de chances de paralyser la cible.";
+
+    const pokemon4 = new Pokemon();
+    pokemon4.name = "Dracofeu";
+    pokemon4.image = "pokemon/dracofeu.jpg";
+    pokemon4.type = PokemonType.FIRE;
+    pokemon4.hp = 150;
+    pokemon4.figureCaption = "N°0006 Dracofeu";
+    pokemon4.attackName = "Canicule";
+    pokemon4.attackStrength = 95;
+    pokemon4.attackDescription = "Canicule a 10% de chances de brûler la cible (si elle n'est pas déjà atteinte par un changement de statut).";
+
     this.pokemons.push(pokemon1);
     this.pokemons.push(pokemon2);
-  }
+    this.pokemons.push(pokemon3);
+    this.pokemons.push(pokemon4);
 
-  inscreaseCount(){
-    this.count++;
-  }
-
-  togglePokemon(){
-    this.selectedPokemonIndex.set((this.selectedPokemonIndex() + 1) % this.pokemons.length);
   }
 }
