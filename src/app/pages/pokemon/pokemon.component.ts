3894cfdc7392828f78f5eb2e75464a3ba289cbac
Gaëtan <dapvril.gaetan@gmail.com>
2025-01-14 14:21:55 +0100

    feat: :sparkles: adding responsive forms and validators

--- src/app/pages/pokemon/pokemon.component.ts
+++ src/app/pages/pokemon/pokemon.component.ts
@@ -1,27 +1,40 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './pokemon.component.html',
   styleUrl: './pokemon.component.scss'
 })
-export class PokemonComponent implements OnInit{
+export class PokemonComponent implements OnInit, OnDestroy{
+  
   private route = inject(ActivatedRoute);
   private router = inject(Router)
-  
+  name = new FormControl('', [Validators.required])
   pokemonId = signal<number | undefined>(undefined)
+  routeSubscription: Subscription | null = null;
 
   ngOnInit(): void {
-    const params = this.route.snapshot.params
+    this.routeSubscription = this.route.params.subscribe(params => {
+      this.pokemonId.set(params['id'] ? parseInt(params['id']) : undefined);
+    })
+  }
 
-    this.pokemonId.set(params['id'] ? parseInt(params['id']) : undefined)
+  ngOnDestroy(): void {
+    this.routeSubscription?.unsubscribe();
   }
 
   next() {
     var nextId = this.pokemonId() || 0;
     nextId++;
     this.router.navigate(['/pokemon/' + nextId])
+  }
+
+  submit(event: Event) {
+    event.preventDefault();
+    console.log(this.name.value);
   }
 }
